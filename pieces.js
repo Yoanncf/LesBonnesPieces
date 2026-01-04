f5034d3a8fd8f227120b5f8f5678f7e17679baa2
Yoann Nicolai <yoann.nicolai@connectingflow.com>
2026-01-04 16:39:36 +0700

    Amélioration de l'interface utilisateur avec des filtres et ajout de la section pour les pièces abordables

--- pieces.js
+++ pieces.js
@@ -2,13 +2,6 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-/// Récupération des boutons
-const boutonFiltrer = document.querySelector(".btn-filtrer");
-const boutonTrier = document.querySelector(".btn-trier");
-const boutonDecroissant = document.querySelector(".btn-decroissant");
-const boutonDescription = document.querySelector(".btn-description");
-
-// Parcours du tableau des pièces automobiles
 for (let i = 0; i < pieces.length; i++) {
   const article = pieces[i];
   // Récupération de l'élément du DOM qui accueillera les fiches
@@ -46,17 +39,27 @@ for (let i = 0; i < pieces.length; i++) {
   pieceElement.appendChild(stockElement);
 }
 
-// Tri des pièces par prix croissant
+//gestion des boutons
+const boutonTrier = document.querySelector(".btn-trier");
+
 boutonTrier.addEventListener("click", function () {
   const piecesOrdonnees = Array.from(pieces);
-
   piecesOrdonnees.sort(function (a, b) {
     return a.prix - b.prix;
   });
   console.log(piecesOrdonnees);
 });
 
-// Tri des pièces par prix décroissant
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+  const piecesFiltrees = pieces.filter(function (piece) {
+    return piece.prix <= 35;
+  });
+  console.log(piecesFiltrees);
+});
+
+const boutonDecroissant = document.querySelector(".btn-decroissant");
 boutonDecroissant.addEventListener("click", function () {
   const piecesOrdonnees = Array.from(pieces);
   piecesOrdonnees.sort(function (a, b) {
@@ -65,17 +68,50 @@ boutonDecroissant.addEventListener("click", function () {
   console.log(piecesOrdonnees);
 });
 
-// Caché des pièces dont le prix est supérieur à 35 €
-boutonFiltrer.addEventListener("click", function () {
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+boutonNoDescription.addEventListener("click", function () {
   const piecesFiltrees = pieces.filter(function (piece) {
-    return piece.prix <= 35;
-  });
-});
-
-// Filtrer les pièces avec description
-boutonDescription.addEventListener("click", function () {
-  const piecesAvecDescription = pieces.filter(function (piece) {
     return piece.description;
   });
-  console.log(piecesAvecDescription);
+  console.log(piecesFiltrees);
 });
+
+const noms = pieces.map((piece) => piece.nom);
+for (let i = pieces.length - 1; i >= 0; i--) {
+  if (pieces[i].prix > 35) {
+    noms.splice(i, 1);
+  }
+}
+
+const description = pieces.map((piece) => piece.description);
+for (let i = pieces.length - 1; i >= 0; i--) {
+  if (pieces[i].prix.description > 35) {
+    description.splice(i, 1);
+  }
+}
+
+//Création de la liste
+const abordablesElements = document.createElement("ul");
+
+//Ajout de chaque nom à la liste
+for (let i = 0; i < noms.length; i++) {
+  const abordablesHeader = description[i];
+  const prix = pieces.map((piece) => piece.prix);
+  const nomElement = document.createElement("li");
+  nomElement.innerText =
+    noms[i] + "- " + prix[i] + "€" + ": " + abordablesHeader;
+  abordablesElements.appendChild(nomElement);
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector(".abordables").appendChild(abordablesElements);
+document.querySelector(".abordables").appendChild(abordablesHeader);
+
+
+const nomsDisponibles = pieces.map((piece) => piece.nom);
+const prixDisponibles = pieces.map((piece) => piece.prix);
+for (let i = pieces.length - 1; i >= 0; i--) {
+  if (!pieces[i].disponibilite) {
+    nomsDisponibles.splice(i, 1);
+    prixDisponibles.splice(i, 1);
+  }
+}
